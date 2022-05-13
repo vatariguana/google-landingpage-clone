dab3c72f5eb31ee1c813d88e8b6aa557bb53bb86
Veronica Atariguana <vatariguanag@gmail.com>
2022-05-13 16:25:20 -0500

    mobile-footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "./views/home";
+import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div className="global">
       <Home/>
     </div>
   );
---
+++ src/components/footer/components/FooterMobile/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const FooterMobile = () => {
+  return (
+    <>
+      <div className="col-12 d-hide contenedorAll-items">
+        <div className=" contenedor-izquierda-mob">
+          <div className="row">
+            <div className="col-6 d-hide items-izquierda-mob">
+              <a href="https://www.google.com/?pccc=1">
+                Tema oscuro:Desactivado
+              </a>
+            </div>
+            <div className="col-6 d-hide items-izquierda-mob">
+              <a href="https://ads.google.com/intl/es-419_ec/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">
+                Preferencias
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 d-hide contenedorAll-items">
+        <div className="contenedor-izquierda-mob">
+          <div className="row">
+            <div className="col-6 items-izquierda-mob">
+              <a href="https://policies.google.com/privacy?hl=es-419&fg=1">
+                Privacidad
+              </a>
+            </div>
+            <div className="col-6 items-izquierda-mob">
+              <a href="https://policies.google.com/terms?hl=es-419&fg=1">
+                Condiciones
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 d-hide contenedorAll-items">
+        <div className="contenedor-derecha-mob">
+          <div className="row">
+            <div className="col-4 items-derecha-mob">
+              <a href="https://ads.google.com/intl/es-419_ec/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1">
+                Publicidad
+              </a>
+            </div>
+            <div className="col-4 items-derecha-mob">
+              <a href="https://smallbusiness.withgoogle.com/intl/es-419/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google&utm_medium=ep&utm_campaign=google_hpbfooter&utm_content=google_hpbfooter&gmbsrc=ww-ww-et-gs-z-gmb-s-z-u~sb-g4sb_srvcs-u&c=EC#!/">
+                Negocios
+              </a>
+            </div>
+            <div className="col-4 items-derecha-mob">
+              <a href="https://about.google/?utm_source=google-EC&utm_medium=referral&utm_campaign=hp-footer&fg=1">
+                Sobre Google
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FooterMobile;
